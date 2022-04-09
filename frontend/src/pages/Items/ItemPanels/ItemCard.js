547704794f48325fc7f2c827361ff76a4132f891
Bayauz <sasha.titov.00@list.ru>
2022-04-09 17:03:56 +0600

    добавил основу запросов

--- frontend/src/pages/Items/ItemPanels/ItemCard.js
+++ frontend/src/pages/Items/ItemPanels/ItemCard.js
@@ -3,13 +3,19 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import {CardActionArea} from '@mui/material';
 
 import adminAvatarSrc from '../../../imgs/admin.jpg'
+import {useEffect} from "react";
+import axios from "axios";
+
 
 const ItemCard = () => {
+
+
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{maxWidth: 345}}>
             <CardActionArea>
                 <CardMedia
                     component="img"
--- frontend/src/pages/Items/Items.js
+++ frontend/src/pages/Items/Items.js
@@ -1,33 +1,69 @@
 import styled from 'styled-components/macro'
 
 import React from 'react';
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
-import ItemCard from "./ItemPanels/ItemCard";
 
-const arr = [1, 2]
+import ItemCard from "./ItemPanels/ItemCard";
+import axios from "axios";
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'http://localhost:8080/api/',
+    crossDomain: true,
+});
 
 
 const Items = () => {
+
+
+    const getData = () => {
+        console.log('ss')
+        instance.get('product/listing',{
+            headers: {
+                "Accept": "application/json",
+                "Access-Control-Allow-Origin": "localhost:8080"
+            },
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+
     return (
-        <Container maxWidth="lg">
-            <Box sx={{bgcolor: "#cfe8fc", height: "100vh"}}>
-
-                {arr.map(el => {
-                    return (
-                        <ItemCardWrapper>
-                            <ItemCard/>
-                        </ItemCardWrapper>
-                    )
-                })}
-
-            </Box>
+        <Container>
+            {arr.map(el => {
+                return (
+                    <ItemCardWrapper>
+                        <div onClick={() => getData()}>ЫЫЫЫ</div>
+                        <ItemCard/>
+                    </ItemCardWrapper>
+                )
+            })}
         </Container>
+
     );
 };
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-content: space-between;
+  align-items: center;
+  flex-direction: row;
+`;
+
 const ItemCardWrapper = styled.div`
-  padding: 25px
+  display: flex;
+  padding: 25px;
+  max-width: 300px;
 `;
 
 export default Items;
