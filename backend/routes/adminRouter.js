3cb1aa57616c11a1531ccbfc80e5d5c26d12d4b7
Artiliart <artiliart251000@gmail.com>
2022-04-25 19:19:56 +0600

    Метод на получение пользователя по айди

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -6,6 +6,7 @@ router.post('/login', adminController.login)
 router.post('/addAdmin', adminController.addAdmin)
 router.patch('/userModify/:id', adminController.userModify)
 router.delete('/delete/:id', adminController.delete)
+router.get('/userId/:id', adminController.userId)
 router.get('/users', adminController.users)
 
 module.exports = router
