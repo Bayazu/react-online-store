181222ce1ba9e2139d65b9e7306f9b76c3e81908
Artiliart <artiliart251000@gmail.com>
2022-06-05 16:37:32 +0600

    сделал несколько методов на заказы

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -4,6 +4,5 @@ const orderController = require('../controllers/orderController')
 
 router.post('/buy', orderController.buy)
 router.post('/orderCreate', orderController.orderCreate)
-router.get('/orders', orderController.orders)
 
 module.exports = router
